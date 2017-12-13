bfbe2350fd92d91c93868bd1c4b5d4f2a46bd981
mekya <ahmetmermerkaya@gmail.com>
2017-12-13 18:32:48 +0300

    Skip gpg sign by default

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mvn clean install -DskipTests
+mvn clean install -DskipTests -Dgpg.skip=true
 cp target/WebRTCAppEE.war ~/softwares/ant-media-server/webapps/
 rm -rf ~/softwares/ant-media-server/webapps/WebRTCAppEE
 cd ~/softwares/ant-media-server/
