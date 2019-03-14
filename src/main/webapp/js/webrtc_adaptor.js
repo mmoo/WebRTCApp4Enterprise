2107a34eda9ba3fef869eae187fb8fb28d7515bf
mmoo <ktgtmn@gmail.com>
2019-03-14 15:52:11 +0000

    Fix 'callbackError is not defined'

--- src/main/webapp/js/webrtc_adaptor.js
+++ src/main/webapp/js/webrtc_adaptor.js
@@ -31,7 +31,7 @@ function WebRTCAdaptor(initialValues)
 
 	if (!("WebSocket" in window)) {
 		console.log("WebSocket not supported.");
-		callbackError("WebSocketNotSupported");
+		thiz.callbackError("WebSocketNotSupported");
 		return;
 	}
 
